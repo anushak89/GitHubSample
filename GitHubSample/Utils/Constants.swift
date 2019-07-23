38c8bab6d705705a3c3c5be5a55c326da1b7c11b
Anusha Kottiyal <kanushak89@gmail.com>
2019-07-23 15:01:02 -0400

    Added Constants file to keep application constants

---
+++ GitHubSample/Utils/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  GitHubSample
+//
+//  Created by Anusha Kottiyal on 7/23/19.
+//  Copyright © 2019 NextDigit. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    enum ViewTitle: String {
+        case commits = "Commits"
+    }
+    struct API {
+        static let baseURL = "https://api.github.com"
+    }
+    
+    struct UI {
+        static let loginFieldsRowHeight: CGFloat = 64
+    }
+}
