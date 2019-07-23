053643eca41353bdd626b8ccae5dcaa12c2a3955
Anusha Kottiyal <kanushak89@gmail.com>
2019-07-23 12:30:05 -0400

    Created basic project structure

---
+++ GitHubSample/Controllers/CommitsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CommitsViewController.swift
+//  GitHubSample
+//
+//  Created by Anusha Kottiyal on 7/23/19.
+//  Copyright © 2019 NextDigit. All rights reserved.
+//
+
+import UIKit
+
+class CommitsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
